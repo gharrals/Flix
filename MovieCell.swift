1888b5497c711a8d585bef1c641e51ff25090b38
gharrals <60074320+gharrals@users.noreply.github.com>
2021-02-01 17:37:20 -0500

    Add files via upload

---
+++ MovieCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieCell.swift
+//  Flix
+//
+//  Created by user190294 on 1/31/21.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
